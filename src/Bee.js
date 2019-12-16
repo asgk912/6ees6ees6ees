53f56c7a2d8c75aa3fff0621d93792603828dd03
Albert <asgk912@gmail.com>
2019-12-16 12:01:58 -0800

    implement Grub in es6 style

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,30 @@
-class Bee {
+class Bee extends Grub{
   // TODO..
+  constructor(){
+  	this.age = 5;
+  	this.color = 'yellow';
+  	this.job = 'Keep on growing'
+  }
 };
+
+
+// Tasks
+	// ES6 style
+	// add following features to the class
+		// extension
+			// the Grub superclass
+		// ineherited properties with unchanged values
+			// a food property that is inherited from grub
+		// inherited properties with changed values
+			// an age property that is set to 5
+			// a color property that is set to yellow
+		// new properties
+			// a job property that is set to Keep on growing
+		// inherited methods
+			// an eat method that is inherited from grub
+		// new method
+			
+		
+		
+		
+		
