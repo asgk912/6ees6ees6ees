219d4cebeb243406b113df1ff7ea130eb9db63e4
Albert <asgk912@gmail.com>
2019-12-16 12:14:22 -0800

    implement HoneyMakerBee class extending from Bee in ES6 style

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,45 @@
-class HoneyMakerBee {
-  // TODO..
+class HoneyMakerBee extends Bee{
+	constructor(){
+		// inherit properties and methods
+		super();
+		// assign new values to inherited properties
+		this.age = 10;
+		this.job = 'make honey';
+		//add new properties
+		this.honeyPot = 0;
+	}
+
+	// new method
+	makeHoney() {
+		this.honeyPot++;
+	}
+	giveHoney() {
+		this.honeyPot--;
+	}
 };
+
+// Tasks
+	// ES6 style
+	// add following features to the class
+		// extension
+			// the Bee superclass
+		// ineherited properties with unchanged values
+			// a color property inherited from bee that is set to yellow
+			// a food property that is inherited from grub
+		// inherited properties with changed values
+			// an age property that is set to 10
+			// a job property that is set to make honey
+		// new properties
+			// a honeyPot property that is set to 0
+		// inherited methods
+			// an eat method that is inherited from grub
+		// new method
+			// a makeHoney method that adds 1 to that honeyBee's honeyPot
+			// a giveHoney method that subtracts 1 from that honeyBee's honeyPot
+
+
+
+
+
+
+
